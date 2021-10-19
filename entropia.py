26b744a09149698676b3eb1040ebdea1e29b24e0
diegojvs <39108875+diegojvs@users.noreply.github.com>
2021-10-19 18:45:35 -0300

    Update entropia.py

--- entropia.py
+++ entropia.py
@@ -5,6 +5,10 @@ import hashlib
 if __name__ == '__main__':
     text = sys.argv[1]
     hashmd5 = hashlib.md5(text.encode()).hexdigest()
-    #print(hashmd5)
-    print('!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~')
-    #print("Entropia de MD5: " + str(Hash.entropia(str(hashmd5),True)))
+    hashsh1 = hashlib.sha1(text.encode()).hexdigest()
+    hashsha256 = hashlib.sha256(text.encode()).hexdigest()
+    hashActividad4 = Hash.hash(text)
+    print("Entropia de MD5: " + str(Hash.entropia(hashmd5)))
+    print("Entropia de sha1: " + str(Hash.entropia(hashsh1)))
+    print("Entropia de sha256: " + str(Hash.entropia(hashsha256)))
+    print("Entropia de Actividad4: " + str(Hash.entropia(hashActividad4)))
